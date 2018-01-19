3af49c0b9cd85b64078c301f314e2c329840bd89
Celande <celande.adrien@gmail.com>
2018-01-19 13:04:40 +0100

    Updating tests

---
+++ gashapon/src/main/java/test/VendingMachineTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class VendingMachineTest extends AbstractVendingMachineTest {
+   
+    public VendingMachine machine;
+
+    public VendingMachineTest() {
+
+    	machine = new VendingMachine();
+    }
+}
