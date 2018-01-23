f137931f1acf4e12859a1d7b8a6a7a17e95de149
beenjyd4 <bpe.kraft@gmail.com>
2018-01-23 16:16:09 +0100

    Begin the Initialization in the MainController + modification of Main GUI

--- gashapon/src/main/java/app/App.java
+++ gashapon/src/main/java/app/App.java
@@ -25,7 +25,7 @@ public class App extends Application {
 		      
 	        final Pane root = (Pane) fxmlLoader.load();
 
-	        final Scene scene = new Scene(root, 350, 300);
+	        final Scene scene = new Scene(root, 1280, 720);
 	        primaryStage.setTitle("Gashapon"); 
 	        primaryStage.setScene(scene);        
 	        primaryStage.show();
@@ -36,26 +36,7 @@ public class App extends Application {
 	}
 
 	public static void main(String[] args) {
-		
-		
-		// create products
-		ArrayList<Product> products = new ArrayList<Product>();
-		products.add(new Product(2,"coke.png","Coke"));
-		products.add(new Product(2.5 ,"sprite.png","Sprite"));
-		products.add(new Product(3,"fanta.png","Fanta"));
-		products.add(new Product(4,"kinder.png","Kinder"));
-		products.add(new Product(3 ,"granola.png","Granola"));
-		products.add(new Product(2,"mars.png","Mars"));
-		products.add(new Product(1.5,"apple_sauce.png","Apple sauce"));
-		products.add(new Product(1 ,"water.png","Water"));
-		products.add(new Product(1.5,"sparkling_water.png","Sparkling water"));
-		//create vending machine
-		try{
-			VendingMachine vendingMachine = new VendingMachine(products);
-		} catch(InitException e) {
-			System.out.println("The products array does not contain enough products (9");
-		}
-		
+			
 		//do stuff now (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
 		System.out.println("do stuff now (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧");
 		launch(args);
--- gashapon/src/main/java/gui/MainController.java
+++ gashapon/src/main/java/gui/MainController.java
@@ -1,17 +1,53 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import exception.InitException;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import vendingMachine.Product;
+import vendingMachine.VendingMachine;
 
 public class MainController implements Initializable {
 
-
+	@FXML
+	private Label id_1;
+	
+	@FXML
+	private Label price_1;
+	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		
+		// create products
+		ArrayList<Product> products = new ArrayList<Product>();
+		products.add(new Product(2,"coke.png","Coke"));
+		products.add(new Product(2.5 ,"sprite.png","Sprite"));
+		products.add(new Product(3,"fanta.png","Fanta"));
+		products.add(new Product(4,"kinder.png","Kinder"));
+		products.add(new Product(3 ,"granola.png","Granola"));
+		products.add(new Product(2,"mars.png","Mars"));
+		products.add(new Product(1.5,"apple_sauce.png","Apple sauce"));
+		products.add(new Product(1 ,"water.png","Water"));
+		products.add(new Product(1.5,"sparkling_water.png","Sparkling water"));
+		//create vending machine
+		try{
+			VendingMachine vendingMachine = new VendingMachine(products);
+		} catch(InitException e) {
+			System.out.println("The products array does not contain enough products (9");
+		}
+		
+		price_1.setText(Double.toString(products.get(0).getPrice())+" €");
+		
+		
+		
+		
+		
 		
 	}
 }
