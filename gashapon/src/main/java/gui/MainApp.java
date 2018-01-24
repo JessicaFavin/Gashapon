9bd45691976c4df763d77600979e606b2acb9c7e
Celande <celande.adrien@gmail.com>
2018-01-24 15:22:02 +0100

    Can update view and show data. There is a problem with the image path

---
+++ gashapon/src/main/java/gui/MainApp.java
@@ -0,0 +1,45 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    //private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        try {
+            // Load the root layout from the fxml file
+            FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("Main.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+			Scene scene = new Scene(page);	        
+			primaryStage.setScene(scene);
+			primaryStage.show();
+        } catch (IOException e) {
+            // Exception gets thrown if the fxml file could not be loaded
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- gashapon/src/main/java/gui/MainController.java
+++ gashapon/src/main/java/gui/MainController.java
@@ -2,22 +2,49 @@ package gui;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 
 import exception.InitException;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import vendingMachine.Product;
 import vendingMachine.VendingMachine;
 
 public class MainController implements Initializable {
 
-	@FXML
-	private Label id_1;
+	@FXML private Label id_1;
+	@FXML private Label id_2;
+	@FXML private Label id_3;
+	@FXML private Label id_4;
+	@FXML private Label id_5;
+	@FXML private Label id_6;
+	@FXML private Label id_7;
+	@FXML private Label id_8;
+	@FXML private Label id_9;
 	
-	@FXML
-	private Label price_1;
+	@FXML private Label price_1;
+	@FXML private Label price_2;
+	@FXML private Label price_3;
+	@FXML private Label price_4;
+	@FXML private Label price_5;
+	@FXML private Label price_6;
+	@FXML private Label price_7;
+	@FXML private Label price_8;
+	@FXML private Label price_9;
+	
+	@FXML private ImageView img_1;
+	@FXML private ImageView img_2;
+	@FXML private ImageView img_3;
+	@FXML private ImageView img_4;
+	@FXML private ImageView img_5;
+	@FXML private ImageView img_6;
+	@FXML private ImageView img_7;
+	@FXML private ImageView img_8;
+	@FXML private ImageView img_9;
 	
 
 	@Override
@@ -39,15 +66,80 @@ public class MainController implements Initializable {
 		try{
 			VendingMachine vendingMachine = new VendingMachine(products);
 		} catch(InitException e) {
-			System.out.println("The products array does not contain enough products (9");
+			System.out.println("The products array does not contain enough products (9)");
 		}
 		
-		price_1.setText(Double.toString(products.get(0).getPrice())+" €");
+		initView(products);
 		
+	}
+	
+	private void initView(ArrayList<Product> products) {
+		String path = "../../../ressources/img/";
 		
+		Iterator<Product> itr = products.iterator();
+		Product product;
 		
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_1.setText(product.getPrice() + " €");
+			id_1.setText("" + product.getId());
+			img_1.setImage(new Image(path + product.getImg()));
+		}
 		
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_2.setText(product.getPrice() + " €");
+			id_2.setText("" + product.getId());
+			img_2.setImage(new Image(path + product.getImg()));
+		}
 		
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_3.setText(product.getPrice() + " €");
+			id_3.setText("" + product.getId());
+			img_3.setImage(new Image(path + product.getImg()));
+		}
 		
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_4.setText(product.getPrice() + " €");
+			id_4.setText("" + product.getId());
+			img_4.setImage(new Image(path + product.getImg()));
+		}
+		
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_5.setText(product.getPrice() + " €");
+			id_5.setText("" + product.getId());
+			img_5.setImage(new Image(path + product.getImg()));
+		}
+		
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_6.setText(product.getPrice() + " €");
+			id_6.setText("" + product.getId());
+			img_6.setImage(new Image(path + product.getImg()));
+		}
+		
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_7.setText(product.getPrice() + " €");
+			id_7.setText("" + product.getId());
+			img_7.setImage(new Image(path + product.getImg()));
+		}
+		
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_8.setText(product.getPrice() + " €");
+			id_8.setText("" + product.getId());
+			img_8.setImage(new Image(path + product.getImg()));
+		}
+
+		if (itr.hasNext()) {
+			product = itr.next();
+			price_9.setText(product.getPrice() + " €");
+			id_9.setText("" + product.getId());
+			img_9.setImage(new Image(path + product.getImg()));
+		}
 	}
 }
